1c2fc87fbdcb694205e1d440fea093b50d37bd08
dv-lebedev <dv.lebedev777@gmail.com>
2026-07-16 10:52:40 +0300

    add Tests proj, enable check PR

---
+++ src/MockService.Tests/UnitTest1.cs
@@ -0,0 +1,16 @@
+﻿namespace MockService.Tests
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void MockTest()
+        {
+            Assert.Pass();
+        }
+    }
+}
